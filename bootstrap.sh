d4fad225b4daa1c2ddb1a402753cf40595c1a458
Jiří Janoušek <janousek.jiri@gmail.com>
2016-08-08 15:32:41 +0200

    bootstrap: $1 specifies env path
    Signed-off-by: Jiří Janoušek <janousek.jiri@gmail.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,10 @@
-if [ ! -d env ]; then
-    pyvenv env
+export PATH="$PATH:/opt/python/bin"
+
+VENV="$1"
+[ ! -z "$VENV" ] || VENV=env
+
+if [ ! -d "$VENV" ]; then
+    pyvenv "$VENV"
 fi
 
-. env/bin/activate && pip3 install -r requirements.txt
+. "$VENV"/bin/activate && pip3 install -r requirements.txt
